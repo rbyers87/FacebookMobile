1f31e9a8e15e3137b967c908209afcf528f93199
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-01-10 13:20:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js')
-        .then(reg => console.log('Service Worker registered!', reg))
-        .catch(err => console.error('Service Worker registration failed:', err));
+        .then(registration => {
+            console.log('[Service Worker] Registered successfully:', registration);
+        })
+        .catch(error => {
+            console.error('[Service Worker] Registration failed:', error);
+        });
 }
